2c3788fe14d856d91b32c5cdff3bccdc7283a326
brunoselva <selva.bruno@gmail.com>
2017-05-23 14:56:02 -0500

    Fixing imports

--- src/main/kotlin/com/github/newsoulbr/controller/GreetingController.kt
+++ src/main/kotlin/com/github/newsoulbr/controller/GreetingController.kt
@@ -1,16 +1,18 @@
 package com.github.newsoulbr.controller
 
 import com.github.newsoulbr.model.Greeting
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 /**
  * Created by brunoselva on 5/23/17.
  */
-@org.springframework.web.bind.annotation.RestController
+@RestController
 class GreetingController {
 
     val counter = java.util.concurrent.atomic.AtomicLong()
 
-    @org.springframework.web.bind.annotation.RequestMapping("/greeting")
+    @RequestMapping("/greeting")
     fun greeting(@org.springframework.web.bind.annotation.RequestParam(value = "name", defaultValue = "World") name:String): Greeting {
         return Greeting(counter.incrementAndGet(), "Hello $name")
     }
